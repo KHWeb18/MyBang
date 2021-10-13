7155c4eeec60ecd3f0795d6b8804efdc77c9f1c9
rudghxodid <rudghxodid@naver.com>
2021-10-13 20:35:52 +0900

    backup

---
+++ backend/khweb/src/main/java/com/mybang/khweb/config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+<<<<<<< HEAD
+package com.example.demo.config;
+=======
+package com.mybang.khweb.config;
+>>>>>>> upstream/main
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+<<<<<<< HEAD
+/* 절대 이부분의 설정을 바꾸지 마십시오!!!!!!!!
+ *  바꾸는 순간 모든 통신이 막히게 됩니다. */
+@EnableWebSecurity
+@Configuration
+=======
+@Configuration
+@EnableWebSecurity
+>>>>>>> upstream/main
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/static/**",
+                "/v2/api-docs", "/configuration/ui", "/swagger-resoureces",
+                "/configuration/security", "/swagger-ui.html", "/webjars/**", "/swagger/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors()
+                .and().csrf().disable()
+                .httpBasic();
+    }
+<<<<<<< HEAD
+}
+=======
+}
+>>>>>>> upstream/main
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <test/>
+</template>
+
+<script>
+import Test from '@/components/Test'
+
+  export default {
+    name: 'Home',
+
+    components: {
+      Test,
+    },
+  }
+</script>
